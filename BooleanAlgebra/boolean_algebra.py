2dd0813a777b40f8f903007ae4a97997518c8e99
28kayak <kaya_ota@msn.com>
2015-05-07 21:06:03 -0700

    implication and exclusice works

--- BooleanAlgebra/boolean_algebra.py
+++ BooleanAlgebra/boolean_algebra.py
@@ -45,9 +45,9 @@ def exclusive(x,y):
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
-    #assert boolean(1, 0, conjunction) == 0, "and"
-    #assert boolean(1, 0, disjunction) == 1, "or"
-    #assert boolean(1, 1, implication) == 1, "material"
+    assert boolean(1, 0, conjunction) == 0, "and"
+    assert boolean(1, 0, disjunction) == 1, "or"
+    assert boolean(1, 1, implication) == 1, "material"
     assert boolean(0, 1, exclusive) == 1, "xor"
     #assert boolean(0, 1, equivalence) == 0, "same?"
     #conjunction(1,0)
